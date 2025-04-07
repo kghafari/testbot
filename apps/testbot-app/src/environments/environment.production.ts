3ad6b6ec8a59801c5329adf1e880732e3ae11f6e
Kyle Ghafari <kyleghafari@gmail.com>
2025-04-06 22:34:04 -0400

    who knows. maybe envs

---
+++ apps/testbot-app/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  testValue: 'prodServer!',
+  environmentName: 'production',
+  production: true,
+};
