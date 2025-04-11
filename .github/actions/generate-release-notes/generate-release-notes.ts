5dade0ae9bf2db3082dd2208d3b985ed5276dc52
Kyle Ghafari <kyleghafari@gmail.com>
2025-04-11 15:01:27 -0400

    just curious if i feed it ts directly lol

---
+++ .github/actions/generate-release-notes/generate-release-notes.ts
@@ -0,0 +1,27 @@
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/action';
+
+export async function runHelloWorld() {
+  try {
+    core.info('👋 Hello from generate-release-notes!');
+    const octokit = new Octokit();
+    const [owner, repo] = 'kghafari/testbot'.split('/');
+
+    // See https://developer.github.com/v3/issues/#create-an-issue
+    const { data } = await octokit.request(
+      'POST /repos/{owner}/{repo}/issues',
+      {
+        owner,
+        repo,
+        title: 'My test issue',
+      }
+    );
+    console.log('Issue created: %s', data.html_url);
+  } catch (error: any) {
+    core.error('❌ An error occurred:');
+    core.error(error.message);
+    core.setFailed(error.message);
+  }
+}
+
+runHelloWorld();
