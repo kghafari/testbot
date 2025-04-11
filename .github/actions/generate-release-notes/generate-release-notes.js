afc213f8e945bfeb1330e24377cc299997519859
Kyle Ghafari <kyleghafari@gmail.com>
2025-04-11 14:22:28 -0400

    just put the built in plac for now

---
+++ .github/actions/generate-release-notes/generate-release-notes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.runHelloWorld = runHelloWorld;
+const tslib_1 = require("tslib");
+const core = tslib_1.__importStar(require("@actions/core"));
+function runHelloWorld() {
+    core.info('👋 Hello from generate-release-notes!');
+}
+//# sourceMappingURL=generate-release-notes.js.map
